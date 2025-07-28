8fe3dc110eae9fcd64f45fcf5bce0163242d4265
shakti <kumarshakti2017@gmail.com>
2025-07-28 08:54:51 +0530

    initial setup completed to start the server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const {PORT} = require("./config/serverConfig");
+
+
+const prepareAndStartServer = async()=>{
+
+    app.use(express.json());
+
+    app.listen(PORT,(error)=>{
+        if(error) {
+            console.log("Error while starting the ReminderService Server");
+            process.exit(1);
+        }
+
+        console.log("Reminder Service started at port: ",PORT);
+    })
+}
+
+prepareAndStartServer();
